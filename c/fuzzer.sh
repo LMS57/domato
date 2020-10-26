c305d79549d559f437d25b7c6daa5d8d9a558a0d
Jarod Keene <jarod.keene@live.com>
2020-10-26 11:44:43 -0500

    changed to logging

--- c/fuzzer.sh
+++ c/fuzzer.sh
@@ -2,6 +2,7 @@
 
 FUZZDIR=/fuzz
 SAVEDIR=/savedtests
+LOG=/fuzzlog
 TIMEOUT=3
 
 savecounter=0
@@ -9,14 +10,11 @@ savecounter=0
 while true; do
 
 	if [ $(ls $FUZZDIR/ | wc -l) -eq 0 ]; then
-		echo "Waiting for more tests..."
 		sleep 2
 		continue
 	fi
 
 	TEST=$(ls $FUZZDIR/ | head -n1)
-
-	echo -n "Testing $TEST: "
 
 
 	OUTPUT=$(timeout -s SIGTERM $TIMEOUT gcc -pass-exit-codes -o /tmp/a.out $FUZZDIR/$TEST 2>&1)
@@ -25,25 +23,25 @@ while true; do
 	if [ $RET -ne 1 ]; then
 
 		if [ $RET -eq 0 ]; then
-			echo "COMPILATION SUCCESS"
+			echo "COMPILATION SUCCESS $TEST" >> $fuzzlog
 
 		elif [ $RET -eq 4 ]; then
-			echo "C FRONTEND COMPILER ERROR"
+			echo "C FRONTEND COMPILER ERROR $TEST" >> $fuzzlog
 
 		elif [ $RET -eq 124 ]; then
-			echo "TIMEOUT"
+			echo "TIMEOUT $TEST" >> $fuzzlog
 
 		elif [ $RET -eq 153 ]; then
-			echo "MEMORY LEAK"
+			echo "MEMORY LEAK $TEST" >> $fuzzlog
 
 		elif [ $RET -eq 255 ]; then
-			echo "EXCEPTION"
+			echo "EXCEPTION $TEST" >> $fuzzlog
 		
 		else
-			echo -e "\e[31;1mUNKNOWN CRASH (ret:$RET)\e[0m"
+			echo -e "\e[31;1mUNKNOWN CRASH (ret:$RET)\e[0m $TEST" >> $fuzzlog
 		fi
 
-		mv $FUZDIR/$TEST $SAVEDIR/$savecounter:$RET.c
+		mv $FUZZDIR/$TEST $SAVEDIR/$savecounter:$RET.c
 		let savecounter=$savecounter+1
 
 	else
@@ -52,7 +50,5 @@ while true; do
 		rm $FUZZDIR/$TEST
 
 	fi
-
-	sleep 0.5
-
+	sleep 0.1
 done
